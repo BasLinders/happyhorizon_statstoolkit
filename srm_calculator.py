8035bf91721d8b16aa78b55386f638c22e61d746
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-06-28 15:30:52 +0200

    Update srm_calculator.py
    imported streamlit package and rewrote input fields.

--- srm_calculator.py
+++ srm_calculator.py
@@ -1,5 +1,6 @@
 try:
     import scipy.stats as stats
+    import streamlit as st
     print("All libraries have been successfully loaded.")
 except ImportError as e:
     print(f"These libraries failed to load: {e}")
@@ -7,16 +8,16 @@ except ImportError as e:
 # Sample data
 while True:
     try:
-        visitors_a = float(input("How many visitors did A have? "))
-        visitors_b = float(input("How many visitors did B have? "))
+        visitors_a = st.number_input("How many visitors did A have? ")
+        visitors_b = st.number_input("How many visitors did B have? ")
         if visitors_a < 0 or visitors_b < 0:
             raise ValueError("Visitor counts cannot be negative.")
         break
     except ValueError as e:
         print(f"Error: {e}. Please enter valid visitor counts.")
 
-proportion_a = int(input("What percentage of users should be in A? "))
-proportion_b = int(input("What percentage of users should be in B? "))
+proportion_a = st.number_input("What percentage of users should be in A? ")
+proportion_b = st.number_input("What percentage of users should be in B? ")
 
 observed = [visitors_a, visitors_b]
 
@@ -36,10 +37,10 @@ else:
     if p_value < 0.01:
         srm_result = (
             f"possible sample ratio mismatch! The distribution of data significantly deviates from the "
-            f"expected proportions of {expected_distribution}. Check the distribution"
+            f"expected proportions of {expected_distribution}. Check the distribution."
         )
     else:
-        srm_result = "valid distribution. The sample proportions do not significantly deviate from the expected split"
+        srm_result = "valid distribution. The sample proportions do not significantly deviate from the expected split."
 
     # Print results
     print(f"p-value: {p_value:.4f}. This suggests a {srm_result}.")
