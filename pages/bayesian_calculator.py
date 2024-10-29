7d95e541bdb68d9b0f5877d0c5698f87829cfbb2
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2024-10-29 15:50:14 +0000

    style fixes conclusive statement

--- pages/bayesian_calculator.py
+++ pages/bayesian_calculator.py
@@ -323,15 +323,15 @@ def run():
                 #st.write(f"Optimistic uplift: €{optimistic_monetary_uplift:.2f}")
 
                 if round(probability_b_better * 100, 2) >= probability_winner:
-                    bayesian_result = "<span style='color: green; font-weight: bold;'>Winner</span>"
+                    bayesian_result = "<span style='color: green; font-weight: bold;'>winner</span>"
                 else:
-                    bayesian_result = "<span style='color: red; font-weight: bold;'>Loss averted</span>"
+                    bayesian_result = "<span style='color: red; font-weight: bold;'>loss averted</span>"
 
                 st.write("### Results Summary")
                 st.write("")
                 st.markdown(
                     f"Variant B has a {round(probability_b_better * 100, 2)}% chance to win with a relative change of {round(observed_uplift,2)}%. "
-                    f"Because your winning threshold was set to {probability_winner}%, this experiment is a {bayesian_result}.",
+                    f"Because your winning threshold was set to {int(probability_winner)}%, this experiment is a {bayesian_result}.",
                     unsafe_allow_html=True
                 )
                 st.write("")
