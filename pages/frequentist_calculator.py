83f1b37fcba139ebd4bb8cfb52bb92c8991f98e4
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2025-02-14 09:09:38 +0000

    placed variants header in loop.

--- pages/frequentist_calculator.py
+++ pages/frequentist_calculator.py
@@ -247,9 +247,9 @@ def run():
                 st.write("The Šidák correction to solve for the Multiple Comparison Problem was applied due to 3 or more variants in the test.")
 
             st.write("## Results summary")
-            st.write(f"### Test results for {alphabet[i]} vs {alphabet[0]}")
             def perform_superiority_test(i, alphabet, p_values, conversion_rates):
                 if significant_results[i - 1]:
+                    st.write(f"### Test results for {alphabet[i]} vs {alphabet[0]}")
                     st.markdown(f" * **Statistically significant result** for {alphabet[i]} with p-value: {p_values[i-1]:.4f} and a power of {observed_powers[i-1] * 100:.2f}%!")
                     st.markdown(f" * **Conversion rate change** for {alphabet[i]}: {((conversion_rates[i] - conversion_rates[0]) / conversion_rates[0]) * 100:.2f}%")
                     if conversion_rates[i] > conversion_rates[0]:
@@ -266,6 +266,7 @@ def run():
                 lower_bound, upper_bound = confidence_interval
 
                 if p_values[i - 1] > sidak_alpha:
+                    st.write(f"### Test results for {alphabet[i]} vs {alphabet[0]}")
                     st.markdown(f" * **Confidence interval for difference in conversion rates:** ({lower_bound:.4f}, {upper_bound:.4f})")
                     st.markdown(f" * **Observed power:** {observed_powers[i-1] * 100:.2f}%")
                     st.markdown(f" * **p-value:** {p_values[i-1]:.4f}")
