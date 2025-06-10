93011b27f75a94fc4abb23770c29f235e7f1e41c
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2025-06-10 15:41:35 +0000

    fixing default choice. setting to two-sided

--- pages/frequentist_analysis.py
+++ pages/frequentist_analysis.py
@@ -18,7 +18,7 @@ def initialize_session_state():
     st.session_state.setdefault("visitor_counts", [0] * st.session_state.num_variants)
     st.session_state.setdefault("variant_conversions", [0] * st.session_state.num_variants)
     st.session_state.setdefault("risk", 0)
-    st.session_state.setdefault("tail", 'greater')
+    st.session_state.setdefault("tail", 'two-sided')
 
 def display_intro():
     st.write("# Frequentist Analysis")
@@ -73,8 +73,8 @@ def get_user_inputs():
 
     st.session_state.tail = st.selectbox(
         "Choose the test type:",
-        options=['two-sided', 'greater', 'less'],
-        index=['two-sided', 'greater', 'less'].index(st.session_state.tail),
+        options=['greater', 'two-sided', 'less'],
+        index=['greater', 'two-sided', 'less'].index(st.session_state.tail),
         help="A one-sided test focuses on a change in one specific direction. Choose 'greater' if you only care whether the variant is significantly better than the control. Choose 'less' if you only care whether the variant is significantly worse than the control. For detecting a significant change in either direction (better or worse), choose 'two-sided'. P-value may be smaller in a one-sided test. Be aware that a one-sided test increases the risk of a Type I error."
     )
 
