ee2fc51bcc511446b4c891621e9f3bdfa864e3fa
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2025-06-19 16:16:23 +0200

    Update main.py
    updating version number.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logo_url = "https://cdn.homerun.co/49305/hh-woordmerk-rgb-blue-met-discriptor166
 st.image(logo_url, width=200)
 
 st.title("Happy Horizon Experimentation Toolkit")
-st.write("### <span style='color: orange;'>v1.6.5 (beta)</span>", unsafe_allow_html=True)
+st.write("### <span style='color: orange;'>v1.6.6 (beta)</span>", unsafe_allow_html=True)
 st.write("""
 This is the main page for the Happy Horizon Experimentation Toolkit. You can navigate to individual apps using the sidebar.
 
@@ -19,8 +19,7 @@ This is the main page for the Happy Horizon Experimentation Toolkit. You can nav
 This toolkit has been created for the purposes of analyzing data from online controlled experiments ('A/B tests') to learn from and better understand user behavior.  
 
 ### Features
-<span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Bayesian analysis**: Calculate the probability of a winner and make a business case<br>
-<span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Frequentist analysis**: Calculate significance for your test variant as tangible proof for a winner<br>
+<span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Experiment analysis**: Use Frequentist and Bayesian methods to analyze test results<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Continuous metric analysis**: Analyze metrics such as revenue / items per transaction<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Experimentation growth estimation**: Calculate annual compound growth potential for experimentation<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Interaction analysis**: Verify if your experiments negatively impacted each other or not<br>
