821a196a5aad0f5b33659fa072230723d951819c
Bas Linders <154208731+BasLinders@users.noreply.github.com>
2025-12-12 16:54:34 +0100

    Update main.py
    Updated version.

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logo_url = "https://cdn.homerun.co/49305/hh-woordmerk-rgb-blue-met-discriptor166
 st.image(logo_url, width=200)
 
 st.title("Happy Horizon Experimentation Toolkit")
-st.write("### <span style='color: orange;'>v0.6.7-beta</span>", unsafe_allow_html=True)
+st.write("### <span style='color: orange;'>v0.6.8-beta</span>", unsafe_allow_html=True)
 st.write("""
 This is the main page for the Happy Horizon Experimentation Toolkit. You can navigate to individual apps using the sidebar.
 
@@ -19,6 +19,7 @@ This is the main page for the Happy Horizon Experimentation Toolkit. You can nav
 This toolkit has been created for the purposes of analyzing data from online controlled experiments ('A/B tests') to learn from and better understand user behavior.  
 
 ### Features
+<span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Behavioral metric analysis**: Analyze higher-level metrics in your experiments<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Experiment analysis**: Use Frequentist and Bayesian methods to analyze test results<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Continuous metric analysis**: Analyze metrics such as revenue / items per transaction<br>
 <span style='color:#009900; font-weight: 600; margin-right: 6px;'>&#10003;</span>**Experimentation growth estimation**: Calculate annual compound growth potential for experimentation<br>
